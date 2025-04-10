f9be5b1bf8050c8261c51614afc79bf88b817d76
Sohan Shanbhag <sohanshanbhag15@gmail.com>
2025-04-10 21:07:09 +0530

    Minor changes in frontend and backend code for dockerizing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
   ],
+  server:{
+    port: 4173,
+    host: true,
+    strictPort: true
+  },
   envPrefix: 'VITE_'
 })
