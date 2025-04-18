46a4594810814f4cdc75120cb9cb7f9b7430359a
Zhenkar Gowda K P <151987799+Zhenkar@users.noreply.github.com>
2025-04-18 23:35:27 +0530

    Zhenkar (#7)
    * Containerised the application
    
    * deployments and services for backend and redis
    
    ---------
    
    Co-authored-by: Sohan Shanbhag <127968647+sohanshanbhag1502@users.noreply.github.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ connectRedis();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", shortenRouter);
+app.use("/", shortenRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
