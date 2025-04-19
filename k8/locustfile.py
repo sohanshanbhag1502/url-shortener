72b7eca809b75511709cb5ec440b7e23d8dc0c82
SHOBITH PS <shobithps5@gmail.com>
2025-04-19 11:03:00 +0530

    Added horizontal pod autoscaler for both frontend and backend, locust file for testing.

---
+++ k8/locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+class UserBehavior(HttpUser):
+    wait_time = between(1, 5)  
+    @task(3)  
+    def load_frontend(self):
+        self.client.get("/")
+
+    @task(1)
+    def load_backend(self):
+        self.client.post("/api/shorten", json={"original": "https://www.google.com"})
+        self.client.post("/api/geturl", json={"shortened": "j3uah"})
